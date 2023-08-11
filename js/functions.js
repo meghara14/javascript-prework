58ee44de8f0520e6bb3fcbbe2d5c5e187fbc8e68
Magdalena Lesisz <meghara@icloud.com>
2023-08-11 19:55:22 +0200

    ustalenie ruchu gracza

--- js/functions.js
+++ js/functions.js
@@ -25,3 +25,24 @@ if (randomNumber == '3') {
 printMessage('Mój ruch:' + computerMove);
 Step JavaScript
 Run JavaScript
+
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
+if (playerInput == '2') {
+  playerMove = 'papier';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch' + playerMove);
+if (playerInput == '3') {
+  playerMove = 'nożyczki';
+} else {
+  playerMove = 'nieznany ruch';
+}
